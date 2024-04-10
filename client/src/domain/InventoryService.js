ea4986fa24951065bc26efd4385095c98f51cfee
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-10 21:22:09 +0200

    chore: created InventoryService

---
+++ client/src/domain/InventoryService.js
@@ -0,0 +1,33 @@
+import ApiService from '../adapters/secondary/ApiService';
+import Article from './models/Article';
+import Order from './models/Order';
+
+export const fetchArticles = async () => {
+  const articles = await ApiService.getArticles();
+  return articles?.map(article =>
+    new Article({
+      id: article.id,
+      name: article.name,
+      description: article.description,
+      price: article.price,
+      taxPercentage: article.tax_percentage
+    })) ?? [];
+};
+
+export const fetchOrders = async () => {
+  const orders = await ApiService.getOrders();
+  return orders?.map(order =>
+    new Order({
+      id: order.id,
+      articles: order.articles,
+    })) ?? [];
+};
+
+export const addArticle = async (name, quantity) => {
+  try {
+    const newArticle = await ApiService.createArticle({ name, quantity });
+    return new Article(newArticle.id, newArticle.name, newArticle.quantity);
+  } catch (error) {
+    console.error("Failed to add article:", error);
+  }
+};
