000242f814ee91ea45fde8003a3fcebf08f9263d
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-11 12:24:11 +0200

    chore: navigation - added link to go back to home from elements screens

--- client/src/screens/ArticlesScreen.jsx
+++ client/src/screens/ArticlesScreen.jsx
@@ -1,8 +1,12 @@
 import { ArticlesList } from '../components/ArticlesList';
+import { Link } from 'react-router-dom';
+import { SCREENS } from '../constants/router';
 
 export const ArticlesScreen = () => {
   return (
     <div className='screen'>
+      <Link to={SCREENS.HOME} className='link'>Back to Home</Link>
+
       <h2>Articles</h2>
       <ArticlesList />
     </div>
--- client/src/screens/OrdersScreen.jsx
+++ client/src/screens/OrdersScreen.jsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom';
+import { SCREENS } from '../constants/router';
 import { OrdersList } from "../components/OrdersList";
 
 export const OrdersScreen = () => {
   return (
     <div className='screen'>
+        <Link to={SCREENS.HOME} className='link'>Back to Home</Link>
+
         <h2>Orders</h2>
         <OrdersList />
     </div>
