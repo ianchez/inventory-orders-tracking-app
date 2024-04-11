871d031f8bd1e7f4455170bf42d423545a4e8ea6
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-11 14:16:32 +0200

    refactor: router constants

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,36 +2,12 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { ROUTES } from "./constants/router";
 
 // styles
 import './App.css';
 
-// constants
-import { SCREENS } from './constants/router';
-
-// screens
-import { HomeScreen } from './screens/HomeScreen';
-import { ErrorScreen } from './screens/ErrorScreen';
-import { ArticlesScreen } from './screens/ArticlesScreen';
-import { OrdersScreen } from './screens/OrdersScreen';
-
-
-const router = createBrowserRouter([
-  {
-    path: SCREENS.HOME,
-    element: <HomeScreen />,
-    errorElement: <ErrorScreen />,
-  },
-  {
-    path: SCREENS.ARTICLES,
-    element: <ArticlesScreen />,
-  },
-  {
-    path: SCREENS.ORDERS,
-    element: <OrdersScreen />,
-  },
-]);
-
+const router = createBrowserRouter(ROUTES);
 
 function App() {
   return (
--- client/src/constants/router.js
+++ client/src/constants/router.js
@@ -1,5 +1,59 @@
+// screens
+import { HomeScreen } from '../screens/HomeScreen';
+import { ErrorScreen } from '../screens/ErrorScreen';
+import { ArticlesScreen } from '../screens/articles/ArticlesScreen';
+import { ArticleItemScreen } from "../screens/articles/ArticleItemScreen";
+import { OrdersScreen } from '../screens/orders/OrdersScreen';
+import { OrderItemScreen } from "../screens/orders/OrderItemScreen";
+
 export const SCREENS = {
-  HOME: '/',
-  ARTICLES: '/articles',
-  ORDERS: '/orders',
+  HOME: {
+    PATH: '/',
+    NAME: 'Home',
+  },
+  ARTICLES: {
+    PATH: '/articles',
+    NAME: 'Articles',
+  },
+  ARTICLE_ITEM: {
+    PATH: '/articles/:id',
+    NAME: 'Article Item',
+  },
+  ORDERS: {
+    PATH: '/orders',
+    NAME: 'Orders',
+  },
+  ORDER_ITEM: {
+    PATH: '/orders/:id',
+    NAME: 'Order Item',
+  },
 };
+
+export const routeBuilder = {
+  ARTICLE: (id = "") => SCREENS.ARTICLE_ITEM.PATH.replace(':id', id),
+  ORDER: (id = "") => SCREENS.ORDER_ITEM.PATH.replace(':id', id),
+};
+
+export const ROUTES = [
+  {
+    path: SCREENS.HOME.PATH,
+    element: <HomeScreen />,
+    errorElement: <ErrorScreen />,
+  },
+  {
+    path: SCREENS.ARTICLES.PATH,
+    element: <ArticlesScreen />,
+  },
+  {
+    path: SCREENS.ARTICLE_ITEM.PATH,
+    element: <ArticleItemScreen />,
+  },
+  {
+    path: SCREENS.ORDERS.PATH,
+    element: <OrdersScreen />,
+  },
+  {
+    path: SCREENS.ORDER_ITEM.PATH,
+    element: <OrderItemScreen />,
+  },
+];
