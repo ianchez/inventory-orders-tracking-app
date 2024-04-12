8d22175cd56b5c884a4a6ed9b4fb086e10b1cee2
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-13 01:47:27 +0200

    chore: added validation and price to orders form

--- client/src/components/ArticleInput.jsx
+++ client/src/components/ArticleInput.jsx
@@ -1,4 +1,8 @@
+import { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
+
+import { InventoryContext } from '../context/inventory';
+import { formatTax, formatPrice } from '../utils/format';
 
 const DEFAULT_ARTICLE_STATE = {
   id: '',
@@ -12,6 +16,9 @@ export const ArticleInput = ({
 }) => {
   const { t } = useTranslation();
 
+  const { articles } = useContext(InventoryContext);
+  const currentArticle = articles.find((a) => a.id === article.id);
+
   const idWarning = article.id === '' || article.id === '0';
   const quantityWarning = article.quantity === '' || article.quantity === '0';
   const articleWarning = idWarning || quantityWarning;
@@ -21,27 +28,44 @@ export const ArticleInput = ({
       key={article.id}
       className={`order-item ${articleWarning ? 'warning' : ''}`}
     >
-      <label className='w45'>
-        {t('articles.formLabel.id')}
-        <input
-          className={idWarning ? 'warning' : ''}
-          type="number"
-          name={`id-${article.id}`}
-          value={article.id}
-          onChange={(e) => handleChangeId({ id: article.id, newId: e.target.value })}
-        />
-      </label>
+      <div>
+        <label className='w45'>
+          {t('articles.formLabel.id')}
+          <input
+            className={idWarning ? 'warning' : ''}
+            type="number"
+            name={`id-${article.id}`}
+            value={article.id}
+            onChange={(e) => handleChangeId({ id: article.id, newId: e.target.value })}
+          />
+        </label>
 
-      <label className='w45'>
-        {t('articles.formLabel.quantity')}
-        <input
-          className={quantityWarning ? 'warning' : ''}
-          type="number"
-          name={`quantity-${article.id}`}
-          value={article.quantity}
-          onChange={(e) => handleChangeQuantity({ id: article.id, quantity: e.target.value })}
-        />
-      </label>
+        <label className='w45'>
+          {t('articles.formLabel.quantity')}
+          <input
+            className={quantityWarning ? 'warning' : ''}
+            type="number"
+            name={`quantity-${article.id}`}
+            value={article.quantity}
+            onChange={(e) => handleChangeQuantity({ id: article.id, quantity: e.target.value })}
+          />
+        </label>
+      </div>
+
+      {currentArticle && (
+        <>
+          <p className='half-left'>
+            {t('articles.itemLabel.tax')} {formatTax(currentArticle.taxPercentage)}
+          </p>
+          <p className='half-right'>
+            {t('articles.itemLabel.unitPrice')} {formatPrice(currentArticle.price)}
+          </p>
+
+          <h3>
+            {t('articles.itemLabel.price')} {formatPrice(currentArticle.price * article.quantity * (1 + currentArticle.taxPercentage / 100))}
+          </h3>
+        </>
+      )}
     </li>
   );
 }
