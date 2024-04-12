3574af2de2ebae6635f14eb0f90335eb8bc5a915
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-12 13:23:05 +0200

    refactor: created articleForm component

---
+++ client/src/components/ArticleForm.jsx
@@ -0,0 +1,128 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
+import { createArticle, updateArticle } from '../domain/InventoryService';
+import { SCREENS } from '../constants/router';
+
+const DEFAULT_FORM_STATE = {
+  ID: "",
+  NAME: "",
+  PRICE: "0",
+  TAX_PERCENTAGE: "0",
+  DESCRIPTION: "",
+};
+
+export const ArticleForm = ({currentArticle, setArticleName, isNewArticle = false}) => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const [formState, setFormState] = useState({
+    id: currentArticle?.id || DEFAULT_FORM_STATE.ID,
+    name: currentArticle?.name || DEFAULT_FORM_STATE.NAME,
+    price: currentArticle?.price || DEFAULT_FORM_STATE.PRICE,
+    taxPercentage: currentArticle?.taxPercentage || DEFAULT_FORM_STATE.TAX_PERCENTAGE,
+    description: currentArticle?.description || DEFAULT_FORM_STATE.DESCRIPTION,
+  });
+
+  const handleSubmitArticle = async (event) => {
+    event.preventDefault();
+    const action = currentArticle ? updateArticle : createArticle;
+    const response = await action(formState);
+    if (response.id) {
+      setFormState(DEFAULT_FORM_STATE);
+      navigate(SCREENS.ARTICLES.PATH)
+    }
+  };
+
+  const handleChange = ({target}) => {
+    // Prevent negative values for price and taxPercentage
+    if (target.name === 'price' || target.name === 'taxPercentage') {
+      if (Number(target.value) < 0) return;
+    }
+
+    if (target.name === 'name') {
+      setArticleName(target.value);
+    }
+
+    setFormState((prevFormState) => ({
+      ...prevFormState,
+      [target.name]: target.value,
+    }));
+  };
+
+  const disableSubmitButton =
+    !formState.name ||
+    !formState.price ||
+    !formState.taxPercentage ||
+    formState.price === '0' ||
+    formState.taxPercentage === '0';
+
+  const subtitle = disableSubmitButton
+    ? t('articles.subtitle.missingDetails')
+    : t('articles.subtitle.details');
+
+  const submitButtonLabel = currentArticle
+    ? t('articles.button.update')
+    : t('articles.button.create');
+
+  return (
+    <form onSubmit={handleSubmitArticle}>
+      <p>{subtitle}</p>
+
+      {!isNewArticle && 
+        <label>
+          {t('articles.formLabel.id')}
+          <input type="number" name="id" value={formState.id} onChange={handleChange} readOnly disabled/>
+        </label>
+      }
+
+      <label>
+        {t('articles.formLabel.name')}
+        <input
+          className={formState.name ? '' : 'warning'}
+          type="text"
+          name="name"
+          value={formState.name}
+          onChange={handleChange}
+        />
+      </label>
+
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: "100%" }}>
+        <label className='w45'>
+          {t('articles.formLabel.price')}
+          <input
+            className={!formState.price || formState.price === '0' ? 'warning' : ''}
+            type="number"
+            name="price"
+            value={formState.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label className='w45'>
+          {t('articles.formLabel.taxPercentage')}
+          <input
+            className={!formState.taxPercentage || formState.taxPercentage === '0' ? 'warning' : ''}
+            type="number"
+            name="taxPercentage"
+            value={formState.taxPercentage}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+
+      <label>
+        {t('articles.formLabel.description')}
+        <textarea
+          name="description"
+          value={formState.description}
+          onChange={handleChange}
+        />
+      </label>
+
+      <button type="submit" className='primary' disabled={disableSubmitButton}>
+        {submitButtonLabel}
+      </button>
+    </form>
+  );
+}
--- client/src/screens/articles/ArticleItemScreen.jsx
+++ client/src/screens/articles/ArticleItemScreen.jsx
@@ -1,150 +1,29 @@
-import { useEffect, useState } from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+import { useParams, Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 import { SCREENS } from '../../constants/router';
 import { useArticles } from '../../adapters/primary/useArticles';
-import { createArticle, updateArticle } from '../../domain/InventoryService';
 
-const DEFAULT_FORM_STATE = {
-  ID: "",
-  NAME: "",
-  PRICE: "0",
-  TAX_PERCENTAGE: "0",
-  DESCRIPTION: "",
-};
+import { ArticleForm } from '../../components/ArticleForm';
 
 export const ArticleItemScreen = () => {
   const { t } = useTranslation();
-  const navigate = useNavigate();
   const params = useParams();
   const { articles } = useArticles();
+
+  const [articleName, setArticleName] = useState('');
+
   const currentArticle = articles.find(article => article.id === params.id);
   const isNewArticle = !currentArticle && params.id === 'new';
 
-  const [formState, setFormState] = useState({
-    id: currentArticle?.id || DEFAULT_FORM_STATE.ID,
-    name: currentArticle?.name || DEFAULT_FORM_STATE.NAME,
-    price: currentArticle?.price || DEFAULT_FORM_STATE.PRICE,
-    taxPercentage: currentArticle?.taxPercentage || DEFAULT_FORM_STATE.TAX_PERCENTAGE,
-    description: currentArticle?.description || DEFAULT_FORM_STATE.DESCRIPTION,
-  });
-
-  useEffect(() => {
-    if (!currentArticle) return;
-
-    setFormState({
-      id: currentArticle.id,
-      name: currentArticle.name,
-      price: currentArticle.price,
-      taxPercentage: currentArticle.taxPercentage,
-      description: currentArticle.description || DEFAULT_FORM_STATE.DESCRIPTION,
-    });
-  }, [currentArticle]);
-
-  const handleCreateArticle = async (event) => {
-    event.preventDefault();
-    const response = await createArticle(formState);
-    console.log(response);
-    setFormState(DEFAULT_FORM_STATE);
-    navigate(SCREENS.ARTICLES.PATH)
-  };
-
-  const handleUpdateArticle = async (event) => {
-    event.preventDefault();
-    const response = await updateArticle(formState);
-    setFormState(response);
-  };
-
-  const handleChange = (event) => {
-    // Prevent negative values for price and taxPercentage
-    if (event.target.name === 'price' || event.target.name === 'taxPercentage') {
-      if (Number(event.target.value) < 0) return;
-    }
-
-    setFormState((prevFormState) => ({
-      ...prevFormState,
-      [event.target.name]: event.target.value,
-    }));
-  };
-
   const screenTitle =
     currentArticle
-      ? formState.name || t('articles.title.singular')
+      ? articleName || t('articles.title.singular')
       : isNewArticle
-        ? formState.name || t('articles.title.new')
+        ? articleName || t('articles.title.new')
         : t('articles.title.notFound');
-
-  const submitButtonAction = currentArticle ? handleUpdateArticle : handleCreateArticle;
-  const submitButtonLabel = currentArticle ? t('articles.button.update') : t('articles.button.create');
-  const disableSubmitButton =
-    !formState.name ||
-    formState.price === '0' ||
-    formState.taxPercentage === '0';
-
-  const subtitle = disableSubmitButton
-    ? t('articles.subtitle.missingDetails')
-    : t('articles.subtitle.details');
-
-  const renderArticleForm = () => (
-    <form onSubmit={submitButtonAction}>
-      <p>{subtitle}</p>
-
-      {!isNewArticle && 
-        <label>
-          {t('articles.formLabel.id')}
-          <input type="number" name="id" value={formState.id} onChange={handleChange} readOnly />
-        </label>
-      }
-
-      <label>
-        {t('articles.formLabel.name')}
-        <input
-          className={formState.name ? '' : 'warning'}
-          type="text"
-          name="name"
-          value={formState.name}
-          onChange={handleChange}
-        />
-      </label>
-
-      <div style={{ display: 'flex', justifyContent: 'space-between', width: "100%" }}>
-        <label className='w45'>
-          {t('articles.formLabel.price')}
-          <input
-            className={formState.price === '0' ? 'warning' : ''}
-            type="number"
-            name="price"
-            value={formState.price}
-            onChange={handleChange}
-          />
-        </label>
-        <label className='w45'>
-          {t('articles.formLabel.taxPercentage')}
-          <input
-            className={formState.taxPercentage === '0' ? 'warning' : ''}
-            type="number"
-            name="taxPercentage"
-            value={formState.taxPercentage}
-            onChange={handleChange}
-          />
-        </label>
-      </div>
-
-      <label>
-        {t('articles.formLabel.description')}
-        <textarea
-          name="description"
-          value={formState.description}
-          onChange={handleChange}
-        />
-      </label>
-
-      <button type="submit" className='primary' disabled={disableSubmitButton}>
-        {submitButtonLabel}
-      </button>
-    </form>
-  );
 
   return (
     <div className='screen'>
@@ -154,7 +33,13 @@ export const ArticleItemScreen = () => {
 
       <h2>{screenTitle}</h2>
 
-      {(currentArticle || isNewArticle) && renderArticleForm()}
+      {(currentArticle || isNewArticle) &&
+        <ArticleForm
+          setArticleName={setArticleName}
+          currentArticle={currentArticle}
+          isNewArticle={isNewArticle}
+        />
+      }
     </div>
   );
 }
