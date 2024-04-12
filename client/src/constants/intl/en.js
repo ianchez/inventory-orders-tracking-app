295203b826f7e17dc49e7986fa33374b75a84357
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-12 12:52:45 +0200

    chore: importing i18n config into react-app and created dictionaries for english and spanish

---
+++ client/src/constants/intl/en.js
@@ -0,0 +1,77 @@
+const enDictionary = {
+  articles: {
+    add: 'Add New Article',
+    button: {
+      create: 'CREATE ARTICLE',
+      update: 'UPDATE ARTICLE',
+    },
+    create: 'Create New Article',
+    pleaseFill: 'Please fill the empty article',
+    itemLabel: {
+      id: 'ID:',
+      name: 'Name:',
+      price: 'Price:',
+      tax: 'Tax:',
+      description: 'Description:',
+    },
+    formLabel: {
+      id: 'Article ID:',
+      description: 'Description:',
+      name: 'Name:',
+      price: 'Price:',
+      quantity: 'Quantity:',
+      taxPercentage: 'Tax %:',
+    },
+    subtitle: {
+      missingDetails: 'Enter the missing details of the article:',
+      details: 'Enter the details of the article:',
+    },
+    title: {
+      new: 'New Article',
+      notFound: 'Article Not Found',
+      singular: 'Article',
+    },
+  },
+  error: {
+    title: 'Oops!',
+    message: 'Sorry, an unexpected error has occurred.',
+  },
+  home: {
+    title: 'Inventory',
+    cards: {
+      articles: 'Articles',
+      orders: 'Orders',
+      totalLabel: 'Total:',
+    },
+  },
+  orders: {
+    button: {
+      create: 'CREATE ORDER',
+      createNew: 'Create New Order',
+      update: 'UPDATE ORDER',
+    },
+    itemLabel: {
+      articles: 'Articles:',
+      articleID: 'Article ID:',
+      id: 'ID:',
+      quantity: 'Quantity:',
+    },
+    new: 'New Order',
+    subtitle: {
+      id: 'ID:',
+      articles: 'Articles:',
+    },
+  },
+  navigation: {
+    backTo: 'Back to',
+  },
+  screens: {
+    Home: 'Home',
+    Articles: 'Articles',
+    Orders: 'Orders',
+    'Article Item': 'Article Item',
+    'Order Item': 'Order Item',
+  },
+};
+
+export default enDictionary;
---
+++ client/src/constants/intl/es.js
@@ -0,0 +1,77 @@
+const esDictionary = {
+  articles: {
+    add: 'Agregar Nuevo Artículo',
+    button: {
+      create: 'CREAR ARTÍCULO',
+      update: 'ACTUALIZAR ARTÍCULO',
+    },
+    create: 'Crear un Nuevo Artículo',
+    pleaseFill: 'Por favor complete el artículo vacío',
+    itemLabel: {
+      id: 'ID:',
+      name: 'Nombre:',
+      price: 'Precio:',
+      tax: 'Impuesto aplicable:',
+      description: 'Descripción:',
+    },
+    formLabel: {
+      id: 'ID Artículo:',
+      description: 'Descripción:',
+      name: 'Nombre:',
+      price: 'Precio:',
+      quantity: 'Cantidad:',
+      taxPercentage: '% de Impuesto Aplicable:',
+    },
+    subtitle: {
+      missingDetails: 'Ingresa los detalles faltantes del artículo:',
+      details: 'Ingresa los detalles del artículo:',
+    },
+    title: {
+      new: 'Nuevo Artículo',
+      notFound: 'Artículo No Encontrado',
+      singular: 'Artículo',
+    },
+  },
+  error: {
+    title: 'Ups!',
+    message: 'Disculpanos, ha ocurrido un error inesperado.',
+  },
+  home: {
+    title: 'Inventario',
+    cards: {
+      articles: 'Artículos',
+      orders: 'Pedidos',
+      totalLabel: 'Total:',
+    },
+  },
+  orders: {
+    button: {
+      create: 'CREAR PEDIDO',
+      createNew: 'Crear Nuevo Pedido',
+      update: 'ACTUALIZAR PEDIDO',
+    },
+    itemLabel: {
+      articles: 'Artículos:',
+      articleID: 'ID Artículo:',
+      id: 'ID:',
+      quantity: 'Cantidad:',
+    },
+    new: 'Nuevo Pedido',
+    subtitle: {
+      id: 'ID:',
+      articles: 'Artículos:',
+    },
+  },
+  navigation: {
+    backTo: 'Volver a',
+  },
+  screens: {
+    Home: 'Inicio',
+    Articles: 'Artículos',
+    Orders: 'Pedidos',
+    'Article Item': 'Artículo',
+    'Order Item': 'Pedido',
+  },
+};
+
+export default esDictionary;
